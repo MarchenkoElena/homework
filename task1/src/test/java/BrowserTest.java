026a543d8fe34ff47efa65cd67fc88e70edc8001
MarchenkoElena <kozyavka520254>
2021-11-20 14:58:45 +0600

    добавлен тест на запуск Chrome и открытие ya.ru

--- task1/src/test/java/BrowserTest.java
+++ task1/src/test/java/BrowserTest.java
@@ -1,2 +1,24 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BrowserTest {
+    private WebDriver driver;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void browserTest() {
+        driver.get("https://ya.ru");
+    }
+
+    @After
+    public void after() {
+        driver.quit();
+    }
 }
